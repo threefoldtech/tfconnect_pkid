2b7170174ebca29ec0519b7b0c9f818cab6eeb48
Maxim Ledaine <maxim.ledaine@outlook.com>
2019-08-29 09:53:12 +0200

    header validation

--- blueprints/v1/modules/verify.py
+++ blueprints/v1/modules/verify.py
@@ -1,12 +1,15 @@
 import base64
 import nacl.encoding
 import nacl.signing
+import json
+import time
 
 def verifySignedData(pk, data):
     try:
         print('/n### --- data verification --- ###')
         print("Verifying data: ", data)
 
+        # signature validation
         decoded_data = base64.b64decode(data)
         print("Decoding data: ", decoded_data)
 
@@ -27,6 +30,7 @@ def verifySignedHeader(pk, header):
         print('/n### --- header verification --- ###')
         print("Verifying header: ", header)
 
+        # signature validation
         decoded_header = base64.b64decode(header)
         print("Decoding header: ", decoded_header)
 
@@ -36,8 +40,21 @@ def verifySignedHeader(pk, header):
         verified_signed_header = verify_key.verify(decoded_header)
         print('verified_signed_header: ', verified_signed_header)
 
+        # timestamp validation 
+        header = json.loads(verified_signed_header)
+
+        milliseconds = time.time() * 1000
+        diff = milliseconds - header['timestamp']
+        print('timestamp difference', diff)
+
         print('### --- END header verification --- ###/n')
 
-        return verified_signed_header
+        # check if diff is less than 5s 
+        # check if intent matches
+        if diff < 5000 and header['intent'] == 'pkid.store':
+            return header
+        
+        # raise exception in case validation fails
+        raise 
     except:
         return None
